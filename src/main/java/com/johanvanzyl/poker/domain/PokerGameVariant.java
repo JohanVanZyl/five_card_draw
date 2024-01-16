5681236b001ba2142161a61b0ee71f0825c02f02
Johan van Zyl <johanvanzyl80@gmail.com>
2024-01-16 13:05:35 +0200

    Refactor poker variant enum
    Refactored the poker variant enum to contain the class
    of the respective enum that implements the variant.
    This will allow us to load variant at runtime when we provide
    the enum name for example PokerVariantName.FIVE_CARD_DRAW will
    load the FiveCardDrawPokerVariant class.

--- src/main/java/com/johanvanzyl/poker/domain/PokerGameVariant.java
+++ src/main/java/com/johanvanzyl/poker/domain/PokerGameVariant.java
@@ -21,6 +21,6 @@ public abstract class PokerGameVariant {
    }
 
    public String getVariantName() {
-      return variantName.name();
+      return this.variantName.toString();
    }
 }
--- src/main/java/com/johanvanzyl/poker/domain/PokerVariantName.java
+++ src/main/java/com/johanvanzyl/poker/domain/PokerVariantName.java
@@ -1,5 +1,15 @@
 package com.johanvanzyl.poker.domain;
 
 public enum PokerVariantName {
-    FIVE_CARD_DRAW, BADUGI, TEXAS_HOLDEM
+    FIVE_CARD_DRAW("com.johanvanzyl.poker.domain.FiveCardDrawPokerGameVariant"), BADUGI("Badugi"), TEXAS_HOLDEM("TexasHoldem");
+
+    private final String variantClassName;
+
+    PokerVariantName(String className) {
+        this.variantClassName = className;
+    }
+
+    public Class<?> getVariantClass() throws ClassNotFoundException {
+        return Class.forName(variantClassName);
+    }
 }
